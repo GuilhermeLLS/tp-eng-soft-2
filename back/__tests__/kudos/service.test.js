a133523bb941fe0371de7ab558a4d2112880111f
Gabriel Chaves <gabriel.chaves@idez.com.br>
2022-12-04 15:53:37 -0300

    Test for repository.insertKudo

--- back/__tests__/kudos/service.test.js
+++ back/__tests__/kudos/service.test.js
@@ -1,5 +1,9 @@
 const service = require('../../src/kudos/service');
-const mockKudos = require('./mocks/kudos.mocks.json')
+const repository = require('../../src/kudos/repository');
+const mockKudos = require('./mocks/kudos.mocks.json');
+
+
+jest.mock('../../src/kudos/repository');
 
 describe('kudos service', () => {
     const kudosService = service.kudoService()
@@ -10,4 +14,16 @@ describe('kudos service', () => {
         expect(kudosCard[2].message).toBe('Segundo');
         expect(kudosCard[3].message).toBe('Primeiro');
     });
+
+    it('Should call insert kudo', () => {
+       const newKudo = {name: 'Kudo1'};
+       const mockedInsertKudo =  jest.fn();
+       repository.kudosRepository.mockImplementation(() => {
+        return {insertKudo: mockedInsertKudo}
+       });
+
+        const kudosCard = kudosService.createKudo(newKudo)
+        expect(mockedInsertKudo).toHaveBeenCalledTimes(1);
+    });
 });
+
