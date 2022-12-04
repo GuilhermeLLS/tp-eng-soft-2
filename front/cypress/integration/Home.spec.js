3bb076c145f36d8addc8fd6f28249a78132814f2
guilhermells <guiluizls@gmail.com>
2022-12-04 19:28:29 -0300

    fix: assertion on e2e

--- front/cypress/integration/Home.spec.js
+++ front/cypress/integration/Home.spec.js
@@ -76,7 +76,7 @@ describe('Testing Home page', () => {
         .eq(0)
         .find('span')
         .eq(2)
-        .should('have.text', 'Para: Lara Silva');
+        .should('have.text', 'Para: lara Silva');
 
       cy.get('article', { timeout: 5000 })
         .eq(0)
