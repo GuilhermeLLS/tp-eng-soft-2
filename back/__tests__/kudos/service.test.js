7602da31c23bffed3798a6313e7eb7094df64ede
augustonildo <augustocarvalhopp@gmail.com>
2022-12-04 16:05:56 -0300

    Remove kudo test

--- back/__tests__/kudos/service.test.js
+++ back/__tests__/kudos/service.test.js
@@ -2,28 +2,36 @@ const service = require('../../src/kudos/service');
 const repository = require('../../src/kudos/repository');
 const mockKudos = require('./mocks/kudos.mocks.json');
 
-
 jest.mock('../../src/kudos/repository');
 
 describe('kudos service', () => {
-    const kudosService = service.kudoService()
-    it('Should return kudos order by asc', () => {
-        const kudosCard = mockKudos.sort(kudosService.sortObjectArrayByDate);
-        expect(kudosCard[0].message).toBe('Quarto');
-        expect(kudosCard[1].message).toBe('Terceiro');
-        expect(kudosCard[2].message).toBe('Segundo');
-        expect(kudosCard[3].message).toBe('Primeiro');
+  const kudosService = service.kudoService();
+  it('Should return kudos order by asc', () => {
+    const kudosCard = mockKudos.sort(kudosService.sortObjectArrayByDate);
+    expect(kudosCard[0].message).toBe('Quarto');
+    expect(kudosCard[1].message).toBe('Terceiro');
+    expect(kudosCard[2].message).toBe('Segundo');
+    expect(kudosCard[3].message).toBe('Primeiro');
+  });
+
+  it('Should call insert kudo', () => {
+    const newKudo = { name: 'Kudo1' };
+    const mockedInsertKudo = jest.fn();
+    repository.kudosRepository.mockImplementation(() => {
+      return { insertKudo: mockedInsertKudo };
     });
 
-    it('Should call insert kudo', () => {
-       const newKudo = {name: 'Kudo1'};
-       const mockedInsertKudo =  jest.fn();
-       repository.kudosRepository.mockImplementation(() => {
-        return {insertKudo: mockedInsertKudo}
-       });
+    kudosService.createKudo(newKudo);
+    expect(mockedInsertKudo).toHaveBeenCalledTimes(1);
+  });
 
-        const kudosCard = kudosService.createKudo(newKudo)
-        expect(mockedInsertKudo).toHaveBeenCalledTimes(1);
+  it('Should call remove kudo', () => {
+    const mockedRemoveKudo = jest.fn();
+    repository.kudosRepository.mockImplementation(() => {
+      return { deleteKudo: mockedRemoveKudo };
     });
+
+    kudosService.removeKudo(1);
+    expect(mockedRemoveKudo).toHaveBeenCalledTimes(1);
+  });
 });
-
